44eed9bc0dbb941afc6dd8a987e9229d99794549
Elant00 <132559849+Elant00@users.noreply.github.com>
2023-12-13 08:41:12 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace föreläsning1DotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
